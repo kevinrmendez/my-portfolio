4f9ffb9935465aa97be466eacc2f85f70983cd81
Kevin <kevinrmendez@gmail.com>
2016-04-18 01:48:53 +0300

    Added files via upload

---
+++ js/fix_nav.js
@@ -0,0 +1,46 @@
+window.onscroll = function(){myFunction()};
+  function isScrolledIntoView(elem){
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return((elemTop>= docViewTop)&&(elemBottom<=docViewBottom))
+   }
+
+
+
+
+  function myFunction(){
+
+    function showNav(){
+    if(document.body.scrollTop>50 || document.documentElement.scrollTop > 50){
+      document.getElementById("nav").className = "fix";
+    }
+    else{
+      document.getElementById("nav").className = "";
+    }
+}
+	function scrollView(){
+    if(isScrolledIntoView(".front-end")){
+   document.getElementById("bar-html").classList.add("html");
+   document.getElementById("bar-css").classList.add("css");
+  document.getElementById("bar-javascript").classList.add("js");
+   document.getElementById("bar-jquery").classList.add("jquery");
+    console.log("class added");
+   }
+   else{
+    console.log("fail")
+   }
+}
+    showNav();
+    scrollView();
+    
+
+
+
+      }
